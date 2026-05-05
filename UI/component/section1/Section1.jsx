73b08d308f3690b1036e0c8924ba5d4191fd5cd0
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-05 23:34:30 +0530

    Create Section1.jsx

---
+++ UI/component/section1/Section1.jsx
@@ -0,0 +1,10 @@
+import Navbar from './Navbar.jsx'
+
+function Section1(){
+    return <>
+    {/* <h1 className="text-red-900">This is Section 1</h1> */}
+    <Navbar />
+    </>
+}
+
+export default Section1;
