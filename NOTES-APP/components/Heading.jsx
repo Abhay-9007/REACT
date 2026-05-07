56c016029663ec6cf928e99d6e5e6e11aa09f838
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-07 14:55:50 +0530

    Create Heading.jsx

---
+++ NOTES-APP/components/Heading.jsx
@@ -0,0 +1,11 @@
+
+
+function Heading(props) {
+    return (
+        <h1 className="text-5xl text-white text-center bg-gray-800 p-2"> 
+            {props.text}
+        </h1>
+    )
+}
+
+export default Heading
