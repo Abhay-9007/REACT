39635432dbf2d2583825e0db6c41f7a955483a07
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-15 11:58:26 +0530

    Create Twinkle_BG.jsx

---
+++ DESIGNS/Twinkle_BG.jsx
@@ -0,0 +1,70 @@
+import RedTwinkle from "./Twinkle_BG/Red.Twinkle"
+import BlueTwinkle from "./Twinkle_BG/Blue.Twinkle"
+import GreenTwinkle from "./Twinkle_BG/Green.Twinkle"
+import GrayTwinkle from "./Twinkle_BG/Gray.Twinkle"
+import IndigoTwinkle from "./Twinkle_BG/Indigo.Twinkle"
+import VioletTwinkle from "./Twinkle_BG/Violet.Twinkle"
+import YellowTwinkle from "./Twinkle_BG/Yellow.Twinkle"
+import BlackTwinkle from "./Twinkle_BG/Black.Twinkle"
+
+
+
+function TwinkleBG() {
+
+    return (
+        <div className="flex flex-wrap h-screen w-screen absolute top-0 left-0 z-0 ">
+            <BlackTwinkle/>
+            <BlackTwinkle/>
+            <BlueTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <BlackTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <BlackTwinkle/>
+            <GreenTwinkle/>
+            <BlueTwinkle/>
+            <RedTwinkle/>
+            <BlueTwinkle/>
+            <BlueTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <IndigoTwinkle/>
+            <BlackTwinkle/>
+            <BlueTwinkle/>
+            <VioletTwinkle/>
+            <GreenTwinkle/>
+            <IndigoTwinkle/>
+            <BlueTwinkle/>
+            <IndigoTwinkle/>
+            <RedTwinkle/>
+            <VioletTwinkle/>
+            <GreenTwinkle/>
+            <IndigoTwinkle/>
+            <IndigoTwinkle/>
+            <BlueTwinkle/>
+            <RedTwinkle/>
+            <BlueTwinkle/>
+            <BlackTwinkle/>
+            <IndigoTwinkle/>
+            <GrayTwinkle/>
+            <IndigoTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <BlackTwinkle/>
+            <BlackTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <IndigoTwinkle/>
+            <YellowTwinkle/>
+            <BlackTwinkle/>
+            <IndigoTwinkle/>
+            <GreenTwinkle/>
+            <RedTwinkle/>
+            <BlueTwinkle/>
+            <GreenTwinkle/>
+        </div>
+    )
+}
+
+export default TwinkleBG
